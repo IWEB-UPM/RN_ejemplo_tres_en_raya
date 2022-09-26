5d1c81f86ec4369ca802462567f505ffce62dc7b
Álvaro Alonso <aalonsog@dit.upm.es>
2022-09-26 18:30:47 +0200

    New version

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import GameScreen from './app/components/GameScreen';
+import GameScreen from './app/GameScreen';
 
 export default function App() {
   return (
